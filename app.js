eee6d660656578e828fdccf7141debf83e55911c
Pedro Paje <pedroaugustotolentinopereira@gmail.com>
2024-02-28 04:10:55 -0300

    Inserir mensagem de erro

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const mensagemErro = document.getElementById("mensagem-erro");
 const textoEntrada = document.getElementById("texto-entrada");
 const textoSaida = document.getElementById("texto-saida");
 const botaoCopiar = document.getElementById("botao-copiar");
@@ -5,15 +6,35 @@ const botaoCopiar = document.getElementById("botao-copiar");
 const criptografia = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u","ufat"]];
 
 function acaoCriptografar() {
+    var regExp = /[a-z]/g;
+    if (textoEntrada.value == "") {
+        mensagemErro.innerText = "! Insira uma mensagem para ser criptografada";
+        return;
+    } else if (!regExp.test(textoEntrada.value)) {
+        mensagemErro.innerText = "! Insira apenas letras minúsculas e sem acento";
+        return;
+    }
+
     let mensagemCriptografada = criptografar(textoEntrada.value);
     textoSaida.value = mensagemCriptografada;
     botaoCopiar.disabled = textoSaida.value == "";
+    mensagemErro.innerText = "";
 }
 
 function acaoDescriptografar() {
+    var regExp = /[a-z]/g;
+    if (textoEntrada.value == "") {
+        mensagemErro.innerText = "! Insira uma mensagem para ser descriptografada";
+        return;
+    } else if (!regExp.test(textoEntrada.value)) {
+        mensagemErro.innerText = "! Insira apenas letras minúsculas e sem acento";
+        return;
+    }
+
     let mensagemDescriptografada = descriptografar(textoEntrada.value);
     textoSaida.value = mensagemDescriptografada;
     botaoCopiar.disabled = textoSaida.value == "";
+    mensagemErro.innerText = "";
 }
 
 function acaoCopiar() {
